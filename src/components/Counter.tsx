89b3763681c57f7ebdf76cb0a7571d0125ef4ac6
Eduard Marbach <mail@eduard-marbach.de>
2024-09-19 22:27:23 +0200

    refactor: update some stuff

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,38 @@
+import { useAction } from "@solidjs/router";
 import { createSignal } from "solid-js";
 import { Button } from "~/components/ui/button";
-import { executeOnServer } from "~/lib/server";
+import {
+  executeOnServer,
+  hello,
+  simulateAction,
+  simulateGetRequest,
+} from "~/lib/server";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
+  const resultAction = useAction(simulateAction);
 
-  const clickHandler = () => {
+  const clickHandler = async () => {
+    const resGet = await simulateGetRequest("hey");
+    console.log(resGet);
+
+    // Not sure how this should be consumed
+    const resHello = await hello("hey");
+    console.log(resHello);
+
     executeOnServer();
     setCount(count() + 1);
   };
-  return <Button onClick={clickHandler}>Clicks: {count()}</Button>;
+
+  const actionHandler = async () => {
+    const resAction = await resultAction("any", { some: "data" });
+    console.log(resAction);
+  };
+
+  return (
+    <div>
+      <Button onClick={clickHandler}>Clicks: {count()}</Button>
+      <Button onClick={actionHandler}>Action please</Button>
+    </div>
+  );
 }
--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,7 +1,33 @@
+import { action, cache, json, redirect } from "@solidjs/router";
+import { GET } from "@solidjs/start";
+
 export const executeOnServer = () => {
   "use server";
 
   console.log("This should run on server");
 
-  return { message: "From Server hello" };
+  return json({ message: "From Server hello" });
 };
+
+export const simulateGetRequest = cache(async (id: string) => {
+  "use server";
+
+  console.log(`simulateGet ${id}`);
+
+  return json({ message: "From Server hello" });
+}, "testget");
+
+export const simulateAction = action(async (id, data) => {
+  "use server";
+  console.log("simulateAction");
+  throw redirect("/", { revalidate: "simulateAction" });
+});
+
+export const hello = GET(async (name: string) => {
+  "use server";
+  console.log(`simulateHello ${name}`);
+  return json(
+    { hello: new Promise<string>((r) => setTimeout(() => r(name), 1000)) },
+    { headers: { "cache-control": "max-age=60" } }
+  );
+});
